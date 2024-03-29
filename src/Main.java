caf72a88d8c28d7c98c37345bdd10e13719e4cf4
CYWZX <cywzx@outlook.com>
2024-03-29 19:16:13 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+ * ClassName: ${NAME}
+ * Package:
+ * Description:
+ * @Author: DSB
+ * @Create: ${DATE} - ${TIME}
+ * @Version: v1.0
+ *
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
